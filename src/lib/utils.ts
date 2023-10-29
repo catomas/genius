6d96be714095a312041318092a7651cf64a96d5c
Juan Camilo Echeverri Salazar <juancamiloecheverrisalazar@Juans-MacBook-Air.local>
2023-10-28 20:47:08 -0500

    vercel_url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,5 +6,8 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function absoluteUrl(path: string) {
-  return `${process.env.NEXT_PUBLIC_APP_URL}${path}`;
+  if (typeof window !== "undefined") return path;
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`;
+
+  return `http://localhost:${process.env.PORT ?? 3000}${path}`;
 }
